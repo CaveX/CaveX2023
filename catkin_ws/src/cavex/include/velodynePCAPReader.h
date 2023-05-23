c539a9021abd0cd0d522f893a82c03960c1be534
Luka Moran <lukapmoran@gmail.com>
2023-05-23 19:10:09 +0930

    Starting velo PCAP/sock reader

---
+++ catkin_ws/src/cavex/include/velodynePCAPReader.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <string>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+
+class velodynePCAPReader {    
+    private:
+        std::string absolutePath;
+        std::vector<char> pcapBuffer;
+        pcl::PointCloud<pcl::PointXYZI>::Ptr pointCloud;        
+
+    public:
+        velodynePCAPReader::velodynePCAPReader(std::string absolutePath);
+        
+        std::string getAbsolutePath() { return absolutePath; }
+
+        std::vector<char> getPCAPBuffer() { return pcapBuffer; }
+
+        void readFile();
+
+        
+};
---
+++ catkin_ws/src/cavex/src/lidar/velodynePCAPReader.cpp
@@ -0,0 +1,10 @@
+#include "velodynePCAPReader.h"
+
+    velodynePCAPReader::velodynePCAPReader(std::string absolutePath) {
+        this->absolutePath = absolutePath;
+    }
+
+    void velodynePCAPReader::readFile() {
+
+    }
+
