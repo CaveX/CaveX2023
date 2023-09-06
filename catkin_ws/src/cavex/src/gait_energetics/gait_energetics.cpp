f02f89df759585c5d3eb640c04b55ee9a1a0bcc1
Riley <rileygroome1@gmail.com>
2023-09-06 12:28:01 +0930

    Chaged print format.

--- catkin_ws/src/cavex/src/gait_energetics/gait_energetics.cpp
+++ catkin_ws/src/cavex/src/gait_energetics/gait_energetics.cpp
@@ -3,27 +3,44 @@
 #include "std_msgs/String.h"
 #include "sensor_msgs/JointState.h"
 #include <ostream>
+#include <iostream>
+#include <fstream>
 
-void writeMsgToLog(const sensor_msgs::JointState msg){
+std_msgs::Int8 gaitType;
+
+void readJointEffort(const sensor_msgs::JointState msg){
     // log messages we are subscribing to
+    // each packet has data for 18 motors
+    std::cout << msg.header;
+    std::cout << "Joint Efforts:" << std::endl;
     for (int i = 0; i < msg.effort.size(); i++){
-        std::cout << msg.effort.at(i) << std::endl;
-        std::cout << "Trying to read" << std::endl;
+        std::cout << msg.effort.at(i) << ";"; // print joint effort
+        //dataFile << std::to_string(msg.effort.at(i));
+        if (msg.effort.at(i) > 0){
+            gaitType.data = 1; // amble
+        }
+        else{
+            gaitType.data = 70; // wave
+        }
     }
-    std::cout << "not in loop " << msg.effort.size() << std::endl;
+    std::cout << std::endl << std::endl;
     // ROS_INFO("The message that we received was: %s", msg.effort);
 }
 
 int main(int argc, char **argv){
     // initalise ros publisher and subscriber node
-    ros::init(argc, argv, "Subscriber");
-    // ros::init(argc, argv, "Publisher");
+    ros::init(argc, argv, "GaitEnergeticsNode");
     ros::NodeHandle nh_sub;
-    // ros::NodeHandle nh_pub;
 
     // intialise ros publisher and subscriber
     ros::Publisher gait_selection = nh_sub.advertise<std_msgs::Int8>("syropod_remote/gait_selection", 1);
-    ros::Subscriber joint_efforts = nh_sub.subscribe("joint_states",1000,writeMsgToLog);
-    ros::spin();
+    ros::Subscriber joint_efforts = nh_sub.subscribe("joint_states",10,readJointEffort);
+    
+    while (ros::ok()){
+        gait_selection.publish(gaitType);
+        
+        ros::spinOnce();
+    }
+    
     return 0;
 }
