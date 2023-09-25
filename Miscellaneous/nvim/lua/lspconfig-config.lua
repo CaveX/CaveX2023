cb46f6d62c34cc514c63c070446b0f1c61a85758
Luka Moran <lukapmoran@gmail.com>
2023-09-25 10:54:42 +0930

    Updated neovim configuration

---
+++ Miscellaneous/nvim/lua/lspconfig-config.lua
@@ -0,0 +1,18 @@
+local lsp = require("lspconfig")
+
+lsp.asm_lsp.setup {}
+lsp.pyright.setup {}
+lsp.tsserver.setup {}
+lsp.rust_analyzer.setup {}
+lsp.html.setup {}
+lsp.cssls.setup {}
+lsp.jsonls.setup {}
+lsp.cmake.setup {}
+lsp.bashls.setup {}
+lsp.lua_ls.setup {}
+lsp.mdx_analyzer.setup {}
+lsp.yamlls.setup {}
+lsp.clangd.setup {}
+lsp.matlab_ls.setup {}
+lsp.vimls.setup {}
+
---
+++ Miscellaneous/nvim/lua/nvim-cmp-config.lua
@@ -0,0 +1,61 @@
+local cmp = require('cmp')
+local lspkind = require('lspkind')
+
+local select_opts = { behavior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+		['<Down>'] = cmp.mapping.select_next_item(select_opts),
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.close(),
+		['<CR>'] = cmp.mapping.confirm({
+			select = true,
+		}),
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
+		{ name = 'path' },
+		{ name = 'buffer' },
+	}, {
+		{ name = 'buffer' },
+	}),
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = 'symbol',
+			maxwidth = 50,
+			ellipsis = '...',
+
+		})
+	}
+})
+
+cmp.setup.cmdline({ '/', '?' }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' },
+	}
+})
+
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' },
+	}, {
+		{ name = 'cmdline' },
+	})
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require('lspconfig')['clangd'].setup {
+	capabilities = capabilities,
+}
